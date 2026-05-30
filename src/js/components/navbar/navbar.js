fc8c21efb6cde7e108ad0c5ea704d3e22b608ce9
khenriqueps <kaiopereira.dev@gmail.com>
2026-05-30 11:04:33 -0300

    feat: serviços estáticos

--- src/js/components/navbar/navbar.js
+++ src/js/components/navbar/navbar.js
@@ -1,7 +1,7 @@
 const navbar = document.getElementById ('navbar');
         
 navbar.innerHTML = `    <nav class="bem-navbar">
-        <a href="#" class="bem-navbar__brand">Kaioba</a>
+        <a href="#" class="bem-navbar__brand">k.henrique</a>
         <input type="checkbox" id="nav-toggle" class="bem-navbar__checkbox">
         <label for="nav-toggle" class="bem-navbar__toggle">☰</label>
         <ul class="bem-navbar__menu">
--- src/js/components/paginas/servicos.js
+++ src/js/components/paginas/servicos.js
@@ -1,5 +1,62 @@
 const servico = `<h1> Esta é a pagina de serviço </h1>
-<p> conheça nossos serviços</p>:`
+<p> conheça nossos serviços</p>:<div class="bem-grid-auto">
+       <div class="bem-card">
+        <img class="bem-card__image" src="image.jpg" alt="Image description">
+        <div class="bem-card__body">
+            <h3 class="bem-card__title">Title</h3>
+            <p>Description</p>
+        </div>
+       </div>
+       <div class="bem-card">
+        <img class="bem-card__image" src="image.jpg" alt="Image description">
+        <div class="bem-card__body">
+            <h3 class="bem-card__title">Title</h3>
+            <p>Description</p>
+        </div>
+       </div>
+       <div class="bem-card">
+        <img class="bem-card__image" src="image.jpg" alt="Image description">
+        <div class="bem-card__body">
+            <h3 class="bem-card__title">Title</h3>
+            <p>Description</p>
+        </div>
+       </div>
+       <div class="bem-card">
+        <img class="bem-card__image" src="image.jpg" alt="Image description">
+        <div class="bem-card__body">
+            <h3 class="bem-card__title">Title</h3>
+            <p>Description</p>
+        </div>
+       </div>
+       <div class="bem-card">
+        <img class="bem-card__image" src="image.jpg" alt="Image description">
+        <div class="bem-card__body">
+            <h3 class="bem-card__title">Title</h3>
+            <p>Description</p>
+        </div>
+       </div>
+       <div class="bem-card">
+        <img class="bem-card__image" src="image.jpg" alt="Image description">
+        <div class="bem-card__body">
+            <h3 class="bem-card__title">Title</h3>
+            <p>Description</p>
+        </div>
+       </div>
+       <div class="bem-card">
+        <img class="bem-card__image" src="image.jpg" alt="Image description">
+        <div class="bem-card__body">
+            <h3 class="bem-card__title">Title</h3>
+            <p>Description</p>
+        </div>
+       </div>
+       <div class="bem-card">
+        <img class="bem-card__image" src="image.jpg" alt="Image description">
+        <div class="bem-card__body">
+            <h3 class="bem-card__title">Title</h3>
+            <p>Description</p>
+        </div>
+       </div>
+`
 
 
 export default servico;
