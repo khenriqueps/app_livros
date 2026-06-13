2b14fbc5a33eaf898ed5a2e99e1183498abf0130
khenriqueps <kaiopereira.dev@gmail.com>
2026-06-13 11:45:41 -0300

    removendo comentarios

--- src/js/main.js
+++ src/js/main.js
@@ -6,9 +6,6 @@ import servico from "./components/paginas/servicos.js";
 
 
 const app = document.getElementById('app');
-// app.textContent = '<h1> Olá mundo </h1>'
-
-// #incio #sobre #contato #servicos
 
 
 let rota = window.location.hash || '#inicio';
@@ -49,8 +46,7 @@ function capturarFormulario(){
 
     formulario.addEventListener('submit', (event) => {
       event.preventDefault();
-      // console.log(event);
-      // const idadeInput = event.target.elements['idade'];
+      
       const idade = document.getElementById('idade');
       if(idade.value >= 65){
          console.log("pessoa com prioridade!");
@@ -68,34 +64,7 @@ function capturarFormulario(){
     } );
    }
 
-//     console.log('contagem regressiva')
-// for(let i =100; i>=0; i-=5){
-//     console.log(i);
-// }
-
-//     console.log('contagem regressiva')
-//     // + soma - subtração / divisão * multiplicação % resto da divisão
-//     // 4 % 2 = 0
-//     // 5 % 2 = 1
-// for (let i=0; i<=100; i++){
-//     if (i % 2 === 0){
-//          console.log(i + 'é par');
-//     }
-    
-// }
-// let temperatura = 20;
-// let ligado = true;
-// while(true){
-//     console.log('a temperatura atual é:' +temperatura);
-//     temperatura += 0.05;
-//     if(temperatura >= 30 && temperatura < 33){
-//         console.log('enviar emais de alerta: temperatura alta!');
-//     }else if(temperatura >= 33){
-//         ligado = false;
-//         console.log('desligando o sistema para evitar danos.');
-//         break;
-//     }  
-// }
+/
 
 
 
